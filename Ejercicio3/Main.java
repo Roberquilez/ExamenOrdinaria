c0d8d511999de39d30ec5ab325e3ccf0f5e053a2
Roberquilez <159033774+Roberquilez@users.noreply.github.com>
2024-05-29 17:11:18 +0200

    mejora del ejercicio3

--- Ejercicio3/Main.java
+++ Ejercicio3/Main.java
@@ -1,5 +1,6 @@
 package Ejercicio3;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,13 @@ class IPODocument {
         this.fecha = fecha;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
 }
 
 class DocumentacionLegal extends IPODocument {
@@ -24,17 +32,9 @@ class DocumentacionLegal extends IPODocument {
         this.registroMercantil = registroMercantil;
     }
 
-}
-class DocumentacionOperativa extends IPODocument {
-    private String operacion;
-    private String procedimiento;
-
-    public DocumentacionOperativa(String nombre, String fecha, String operacion, String procedimiento) {
-        super(nombre, fecha);
-        this.operacion = operacion;
-        this.procedimiento = procedimiento;
+    public void printDetails() {
+        System.out.println("Nombre: " + getNombre() + ", Fecha: " + getFecha() + ", Abogado Encargado: " + abogadoEncargado + ", Registro Mercantil: " + registroMercantil);
     }
-
 }
 
 class DocumentacionFinanciera extends IPODocument {
@@ -45,6 +45,10 @@ class DocumentacionFinanciera extends IPODocument {
         super(nombre, fecha);
         this.balance = balance;
         this.estadoResultados = estadoResultados;
+    }
+
+    public void printDetails() {
+        System.out.println("Nombre: " + getNombre() + ", Fecha: " + getFecha() + ", Balance: " + balance + ", Estado de Resultados: " + estadoResultados);
     }
 }
 
@@ -59,6 +63,9 @@ class HitosAvancesIPO {
         this.estado = estado;
     }
 
+    public void printDetails() {
+        System.out.println("Descripcion: " + descripcion + ", Fecha: " + fecha + ", Estado: " + estado);
+    }
 }
 
 class Proveedor {
@@ -70,6 +77,9 @@ class Proveedor {
         this.contacto = contacto;
     }
 
+    public void printDetails() {
+        System.out.println("Nombre: " + nombre + ", Contacto: " + contacto);
+    }
 }
 
 class Inversionista {
@@ -81,7 +91,9 @@ class Inversionista {
         this.inversionRealizada = inversionRealizada;
     }
 
-    // Otros métodos específicos para los inversionistas
+    public void printDetails() {
+        System.out.println("Nombre: " + nombre + ", Inversion Realizada: " + inversionRealizada);
+    }
 }
 
 class ActorIPO {
@@ -93,17 +105,67 @@ class ActorIPO {
         this.rol = rol;
     }
 
+    public void printDetails() {
+        System.out.println("Nombre: " + nombre + ", Rol: " + rol);
+    }
+}
+
+class DocumentacionOperativa extends IPODocument {
+    private String operacion;
+    private String procedimiento;
+
+    public DocumentacionOperativa(String nombre, String fecha, String operacion, String procedimiento) {
+        super(nombre, fecha);
+        this.operacion = operacion;
+        this.procedimiento = procedimiento;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         DocumentacionLegal docLegal = new DocumentacionLegal("Contrato de IPO", "2024-05-29", "Abogado X", "Registro123");
         DocumentacionFinanciera docFinanciera = new DocumentacionFinanciera("Balance General", "2024-05-29", 1000000.0, 500000.0);
-        HitosAvancesIPO hito1 = new HitosAvancesIPO("Presentación ante la SEC", "2024-05-30", "En progreso");
         Proveedor proveedor1 = new Proveedor("Proveedor A", "contacto@proveedora.com");
         Inversionista inversionista1 = new Inversionista("Inversionista B", 200000.0);
         ActorIPO actor1 = new ActorIPO("Consultor C", "Consultoría legal");
 
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("1. Imprimir documentación legal");
+            System.out.println("2. Imprimir documentación financiera");
+            System.out.println("3. Imprimir proveedores");
+            System.out.println("4. Imprimir inversionistas");
+            System.out.println("5. Imprimir actores IPO");
+            System.out.println("6. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    docLegal.printDetails();
+                    break;
+                case 2:
+                    docFinanciera.printDetails();
+                    break;
+                case 3:
+                    proveedor1.printDetails();
+                    break;
+                case 4:
+                    inversionista1.printDetails();
+                    break;
+                case 5:
+                    actor1.printDetails();
+                    break;
+                case 6:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, elige una opción del 1 al 6.");
+                    break;
+            }
+        } while (opcion != 6);
+
+        scanner.close();
     }
 }
-
